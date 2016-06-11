9c698584e07dfefc696322959a4aa8b79b009118
Jaiveer Singh <jsboygenius@gmail.com>
2016-06-11 14:47:17 -0700

    Change servos to non continous

--- mainArmProgram/mainArmProgram.ino
+++ mainArmProgram/mainArmProgram.ino
@@ -2,35 +2,57 @@
 
 Servo servoBase;
 Servo servoArm;
+Servo servoForearm;
 
 int vacuumPin = 8;
 int servoBasePin = 9;
 int servoArmPin = 10;
+int servoForearmPin = 11;
 
-int deg90 = 400;
+int base90 = 300;
+
+int armLowered = 0;
+int armRaised = 90;
+
+int forearmLowered = 0;
+int forearmRaised = 90;
 
 int currBasePos = 0;
-
-void testTurn() {
-  servoBase.write(180);
-  delay(deg90);
-  servoBase.write(90);
-}
 
 void baseTurn(int baseTurnDirection, int baseTurnDegrees)
 {
   servoBase.write(90 + baseTurnDirection * 90);
-  delay(1600);
+  delay(baseTurnDegrees / 90 * base90);
   servoBase.write(90);
 }
+
+void armTurn(int armTurnDegrees)
+{
+  servoArm.write(armTurnDegrees);
+}
+
+void forearmTurn(int forearmTurnDegrees)
+{
+  servoForearm.write(forearmTurnDegrees);
+}
+
+void armDown()
+{
+  while(servoArm.read() > armLowered || )
+}
+
 
 void setup() {
   servoBase.attach(servoBasePin);
   servoBase.write(90);
   servoArm.attach(servoArmPin);
   servoArm.write(90);
+  servoForearm.attach(servoForearmPin);
   pinMode(vacuumPin, OUTPUT);
   baseTurn(1, 360);
+  baseTurn(-1, 360);
+  armTurn(armRaised);
+  armTurn(armLowered);
 }
 
 void loop()
