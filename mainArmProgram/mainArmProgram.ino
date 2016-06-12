e4fe838024d8115c924b9b93e776e30415873600
Jaiveer Singh <jsboygenius@gmail.com>
2016-06-11 17:28:29 -0700

    Skeleton completed

--- mainArmProgram/mainArmProgram.ino
+++ mainArmProgram/mainArmProgram.ino
@@ -9,9 +9,9 @@ int servoBasePin = 9;
 int servoArmPin = 10;
 int servoForearmPin = 11;
 
-int base90 = 300;
+int base90 = 250;
 
-int armLowered = 90;
+int armLowered = 95;
 int armRaised = 50;
 
 int forearmLowered = 60;
@@ -23,8 +23,8 @@ int currBasePos = 0;
 
 void baseTurn(int baseTurnDirection, int baseTurnDegrees)
 {
-  servoBase.write(90 + baseTurnDirection * 90);
-  delay(baseTurnDegrees / 90 * base90);
+  servoBase.write(90 + (baseTurnDirection * -45));
+  delay(2 * baseTurnDegrees / 90 * base90);
   servoBase.write(90);
 }
 
@@ -46,7 +46,11 @@ void armDown()
   {
     servoArm.write(map(i, 0, 100, armInitial, armLowered));
     servoForearm.write(map(i, 0, 100, forearmInitial, forearmLowered));
+    delay(20);
   }
+  servoArm.write(armLowered);
+  servoForearm.write(forearmLowered);
+  delay(500);
 }
 
 void armUp()
@@ -57,8 +61,70 @@ void armUp()
   {
     servoArm.write(map(i, 0, 100, armInitial, armRaised));
     servoForearm.write(map(i, 0, 100, forearmInitial, forearmRaised));
+    delay(20);
   }
+  servoArm.write(armRaised);
+  servoForearm.write(forearmRaised);
+  delay(500);
 }
+
+void vacuumOn()
+{
+  digitalWrite(vacuumPin, HIGH);
+}
+
+void vacuumOff()
+{
+  digitalWrite(vacuumPin, LOW);
+}
+
+void mainRoutine()
+{
+  //Initial Setup
+  vacuumOff();
+  //Grab first slice of bread
+  armUp();
+  baseTurn(1, 90);
+  armDown();
+  vacuumOn();
+  //Drop first slice of bread
+  armUp();
+  baseTurn(-1, 180);
+  armDown();
+  vacuumOff();/*
+  //Grab meat
+  armUp();
+  baseTurn(1, 120);
+  armDown();
+  vacuumOn();
+  //Drop meat
+  armUp();
+  baseTurn(-1, 210);
+  armDown();
+  vacuumOff();
+  //Grab cucumber
+  armUp();
+  baseTurn(1, 150);
+  armDown();
+  vacuumOn();
+  //Drop cucumber
+  armUp();
+  baseTurn(-1, 240);
+  armDown();
+  vacuumOff();
+  //Grab second slice of bread
+  armUp();
+  baseTurn(1, 90);
+  armDown();
+  vacuumOn();
+  //Drop second slice of bread
+  armUp();
+  baseTurn(-1, 180);
+  armDown();
+  vacuumOff();*/
+}
+
+
 
 void setup() {
   servoBase.attach(servoBasePin);
@@ -66,15 +132,9 @@ void setup() {
   servoArm.attach(servoArmPin);
   servoForearm.attach(servoForearmPin);
   pinMode(vacuumPin, OUTPUT);
-  
+  mainRoutine();
 }
 
 void loop()
 {
-  servoArm.write(armLowered);
-  servoForearm.write(forearmLowered);
-  delay(1000);
-  servoArm.write(armRaised);
-  servoForearm.write(forearmRaised);
-  delay(5000);
 }
